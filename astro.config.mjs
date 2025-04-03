41695e10e305fd2d355e653584db6ed97364a352
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-04-03 11:46:09 +0100

    testing hybrid ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
   adapter: vercel(),
 });
